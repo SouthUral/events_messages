f919256b615756d9a52b7144d860744d408b9048
Vladimir <kovalenko@polymetal.ru>
2023-04-10 16:37:21 +0300

    Сервис подключается к postgres и отправляет туда сообзение

--- messDeliv/main.go
+++ messDeliv/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 
+	pgx "github.com/jackc/pgx/v5"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -20,30 +22,65 @@ type Rabbit struct {
 	RabbitConn RabbitConn
 }
 
+type Postgres struct {
+	Host         string
+	Port         string
+	User         string
+	Password     string
+	DataBaseName string
+	Conn         *pgx.Conn
+}
+
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
 	}
 }
 
-func (rabbit *Rabbit) fillingFieldsEnv() {
-	rabbit.Host = rabbit.getEnvStr("ASD_RMQ_HOST", "localhost")
-	rabbit.Port = rabbit.getEnvStr("ASD_RMQ_PORT", "5672")
-	rabbit.VHost = rabbit.getEnvStr("ASD_RMQ_VHOST", "")
-	rabbit.User = rabbit.getEnvStr("SEVICE_RMQ_ENOTIFY_USERNAME", "")
-	rabbit.Password = rabbit.getEnvStr("SEVICE_RMQ_ENOTIFY_PASSWORD", "")
-	rabbit.Heartbeat = rabbit.getEnvInt("ASD_RMQ_HEARTBEAT", 1)
-	rabbit.NameQueue = rabbit.getEnvStr("ASD_RMQ_NAME_QUEUE", "")
+func (pg *Postgres) pgEnv() {
+	pg.Host = getEnvStr("ASD_POSTGRES_HOST", "localhost")
+	pg.Port = getEnvStr("ASD_POSTGRES_PORT", "5432")
+	pg.User = getEnvStr("ASD_POSTGRES_USER", "postgres")
+	pg.Password = getEnvStr("ASD_POSTGRES_PASSWORD", "postgres")
+	pg.DataBaseName = getEnvStr("ASD_POSTGRES_DBNAME", "postgres")
 }
 
-func (rabbit Rabbit) getEnvStr(key, defaultVal string) string {
+func (pg *Postgres) connPg() {
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", pg.User, pg.Password, pg.Host, pg.Port, pg.DataBaseName)
+	var err error
+	pg.Conn, err = pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+func (pg *Postgres) requestDb(msg []byte) {
+	_, err := pg.Conn.Exec(context.Background(), "call device.set_messages($1)", msg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func (rabbit *Rabbit) rabbitEnv() {
+	rabbit.Host = getEnvStr("ASD_RMQ_HOST", "localhost")
+	rabbit.Port = getEnvStr("ASD_RMQ_PORT", "5672")
+	rabbit.VHost = getEnvStr("ASD_RMQ_VHOST", "")
+	rabbit.User = getEnvStr("SEVICE_RMQ_ENOTIFY_USERNAME", "")
+	rabbit.Password = getEnvStr("SEVICE_RMQ_ENOTIFY_PASSWORD", "")
+	rabbit.Heartbeat = getEnvInt("ASD_RMQ_HEARTBEAT", 1)
+	rabbit.NameQueue = getEnvStr("ASD_RMQ_NAME_QUEUE", "")
+}
+
+func getEnvStr(key, defaultVal string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
 	return defaultVal
 }
 
-func (rabbit Rabbit) getEnvInt(key string, defaultVal int) int {
+func getEnvInt(key string, defaultVal int) int {
 	if value, exists := os.LookupEnv(key); exists {
 		val, err := strconv.Atoi(value)
 		if err != nil {
@@ -54,7 +91,7 @@ func (rabbit Rabbit) getEnvInt(key string, defaultVal int) int {
 	return defaultVal
 }
 
-func (rabbit *Rabbit) Connection() {
+func (rabbit *Rabbit) connRabbit() {
 	loginParameters := fmt.Sprintf("amqp://%s:%s@%s:%s/%s", rabbit.User, rabbit.Password, rabbit.Host, rabbit.Port, rabbit.VHost)
 	var err error
 	rabbit.RabbitConn.Connector, err = amqp.Dial(loginParameters)
@@ -89,14 +126,19 @@ type RabbitConn struct {
 
 func main() {
 	configRabbit := Rabbit{}
-	configRabbit.fillingFieldsEnv()
-	configRabbit.Connection()
+	configRabbit.rabbitEnv()
+	configRabbit.connRabbit()
+
+	confPg := Postgres{}
+	confPg.pgEnv()
+	confPg.connPg()
 
 	m, err := configRabbit.Consumer()
 	failOnError(err, "Failed to register a consumer")
 
 	for d := range m {
-		log.Printf("Received a message: %s", d.Body)
+		log.Printf("Received a message")
+		confPg.requestDb(d.Body)
 		d.Ack(true)
 	}
 
